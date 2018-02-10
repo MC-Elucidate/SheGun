ca86ec3a0248ec439d3140866a51b39ba8842824
Mohamed Carrim <crrmoh003@myuct.ac.za>
2018-02-10 22:37:16 +0200

    Suspended platforms
    Added suspended platforms that can be lowered by cutting the suspenders
    with gun datsu

---
+++ Assets/Scripts/Environment/Platforms/SuspendedPlatform.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuspendedPlatform : MonoBehaviour, IEntityWithWeakpoint {
+
+    [SerializeField]
+    private int numberOfSuspenders;
+
+    private new Rigidbody2D rigidbody;
+
+    public void WeakpointHit(WeakPoint weakpointThatWasHit)
+    {
+        numberOfSuspenders -= 1;
+
+        if (numberOfSuspenders == 0)
+            ReleaseSuspension();
+    }
+
+    void Start () {
+        rigidbody = GetComponent<Rigidbody2D>();
+        rigidbody.bodyType = RigidbodyType2D.Kinematic;
+    }
+
+    private void ReleaseSuspension()
+    {
+        rigidbody.bodyType = RigidbodyType2D.Dynamic;
+    }
+	
+}
