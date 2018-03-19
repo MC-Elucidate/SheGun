29c0921068ad3a9516e5b4b0a48f502e5a2085d3
Mohamed Carrim <crrmoh003@myuct.ac.za>
2018-03-19 19:56:11 +0200

    Projectile enemy tweaks
    Projectile enemies have a player sensor for when they should fire, and
    only fire in a line parallel to the x axis.

--- Assets/Scripts/Enemies/ProjectileEnemyStatusManager.cs
+++ Assets/Scripts/Enemies/ProjectileEnemyStatusManager.cs
@@ -10,9 +10,12 @@ public class ProjectileEnemyStatusManager : EnemyStatusManager {
     [SerializeField]
     private float timeBetweenAttacks = 2f;
 
+    private PlayerSensor playerSensor;
+
     protected override void Start()
     {
         base.Start();
+        playerSensor = GetComponentInChildren<PlayerSensor>();
         StartCoroutine(FireProjectile());
     }
 
@@ -21,8 +24,12 @@ public class ProjectileEnemyStatusManager : EnemyStatusManager {
         while (Health > 0)
         {
             yield return new WaitForSecondsRealtime(timeBetweenAttacks);
+            if (!playerSensor.playerInRange)
+                continue;
             EnemyProjectile projectile = Instantiate(projectilePrefab, transform.position, Quaternion.identity).GetComponent<EnemyProjectile>();
-            projectile.TargetPosition = player.transform.position;
+            Vector2 playerDirection = DirectionHelper.GetDirectionVector((player.transform.position - transform.position).x > 0 ? EDirection.Right : EDirection.Left);
+            print(playerDirection);
+            projectile.TargetPosition = transform.position + new Vector3(playerDirection.x, playerDirection.y, 0);
         }
     }
 
